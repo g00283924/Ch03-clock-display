02fee73e84eba7e156bb7732f134d85d84fbb095
Joan Rosario <g00283924@stu.raritanval.edu>
2021-02-10 11:26:05 -0500

    ClockDisplay12Hour
    version 1

--- ClockDisplay12Hour.java
+++ ClockDisplay12Hour.java
@@ -12,7 +12,7 @@
  * @author Michael Kölling and David J. Barnes
  * @version 2016.02.29
  */
-public class ClockDisplay
+public class ClockDisplay12Hour
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
@@ -22,9 +22,9 @@ public class ClockDisplay
      * Constructor for ClockDisplay objects. This constructor 
      * creates a new clock set at 00:00.
      */
-    public ClockDisplay()
+    public ClockDisplay12Hour()
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
         updateDisplay();
     }
@@ -34,10 +34,10 @@ public class ClockDisplay
      * creates a new clock set at the time specified by the 
      * parameters.
      */
-    public ClockDisplay(int hour, int minute)
+    public ClockDisplay12Hour(int hour, int minute)
     {
-        hours = new NumberDisplay(12);
-        minutes = new NumberDisplay(59);
+        hours = new NumberDisplay(13);
+        minutes = new NumberDisplay(60);
         setTime(hour, minute);
     }
 
